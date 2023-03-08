e769973036f5c5dcc7f4dc5357194757a9dda801
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2023-03-08 16:23:23 -0700

    Initial Commit-- Working on basic splashcreen with logotype recreated in code

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Baskerville: require("./assets/LibreBaskerville-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View onLayout={onLayoutRootView} style={styles.container}>
+      <StatusBar style="light" />
+      <View style={styles.logo}>
+        <Text>
+          <Text style={styles.textGrey}>The </Text>
+          <Text style={styles.textBlack}>Met</Text>
+        </Text>
+        <Text>
+          <Text style={styles.textGrey}>ropolitan</Text>
+        </Text>
+        <Text>
+          <Text style={styles.textBlack}>Opera</Text>
+        </Text>
+      </View>
+      {/* <View style={styles.headerContainer}>
+        <Text style={styles.subheader}>IL</Text>
+        <Text style={styles.heading}>TROVATORE</Text>
+      </View> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  headerContainer: {
+    marginTop: 60,
+  },
+  subheader: {
+    fontFamily: "Baskerville",
+    fontSize: 29.97,
+    color: "#eee",
+  },
+  heading: {
+    fontFamily: "Baskerville",
+    fontSize: 48.5,
+    color: "#eee",
+  },
+  textGrey: {
+    color: "#999",
+    fontSize: 30,
+    fontFamily: "Baskerville",
+  },
+  textBlack: {
+    color: "#eee",
+    fontSize: 30,
+    fontFamily: "Baskerville",
+  },
+  headingContainer: {
+    padding: 1,
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#232323",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
